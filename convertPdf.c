a6a1dab813ef643a2784ef0019e34f6d2222a267
bilalltr <bilal.latrach2000@gmail.com>
2022-12-06 22:49:52 +0100

    Sprint 4

--- convertPdf.c
+++ convertPdf.c
@@ -3,7 +3,7 @@
 #include <string.h>
 #include <unistd.h>
 #include <sys/wait.h>
-#define TAILLE_MAX 100000
+#define TAILLE_MAX 1000000000
 
 void listerPdf(){
    
@@ -23,7 +23,7 @@ void listerPdf(){
               parcourir le fichier "tst.txt" et à chaque fois on prend le non d'un
               fichier pdf et on le converti à un fichier.txt on gardant leur nom
         */
-        while (fgets(chaine, TAILLE_MAX, fichier) != NULL) 
+        while(fgets(chaine, TAILLE_MAX, fichier) != NULL) 
         {
             snprintf(ch,TAILLE_MAX,"pdftotext listepdf/%s",chaine);
             system(ch);
@@ -62,6 +62,36 @@ void listerPdfWithOptionLayout(){
         fclose(fichier);
     
         system("mv listepdf/*.txt resultatLayout/"); // deplacer les fichier.txt dans le dossier resultat
+        system("rm tst.txt"); // supprimer le fichier "tst.txt"
+}
+
+void listerPdfWithOptionRaw(){
+   
+	   char * ch;
+	   ch = (char *) malloc(TAILLE_MAX * sizeof(char));
+	   
+	   char * chaine; 
+	   chaine = (char *) malloc(TAILLE_MAX * sizeof(char));
+	   
+	   system("ls listepdf/>tst.txt "); // mettre les non des fichier.pdf dans un fichier "tst.txt"
+	   system("rm -fr resultatRaw");  // supprimer le dossier resultat si il existe
+	   system("mkdir resultatRaw");  // créer un nouveau dossier resultat
+  
+        // on ouvre le fichier "tst.txt"
+        FILE* fichier = fopen("tst.txt", "r");
+        /*
+              parcourir le fichier "tst.txt" et à chaque fois on prend le non d'un
+              fichier pdf et on le converti à un fichier.txt on gardant leur nom
+        */
+        while (fgets(chaine, TAILLE_MAX, fichier) != NULL) 
+        {
+            snprintf(ch,TAILLE_MAX,"pdftotext -raw listepdf/%s",chaine);
+            system(ch);
+        }
+ 
+        fclose(fichier);
+    
+        system("mv listepdf/*.txt resultatRaw/"); // deplacer les fichier.txt dans le dossier resultat
         system("rm tst.txt"); // supprimer le fichier "tst.txt"
 }
 
@@ -144,44 +174,6 @@ char * trouver_abstract(char * fichier){
         char * newLigne;
         newLigne = (char*)malloc(TAILLE_MAX * sizeof(char));
         
-         /*  if((strstr(ligne, "Introduction") || strstr(ligne, "INTRODUCTION") || strstr(ligne, "introduction")||strstr(ligne, "I NTRODUCTION")) && condition==0){
-                     char * ch;
-                     ch = (char*)malloc(TAILLE_MAX * sizeof(char));
-                     
-                     char * pars;
-                     pars = (char*)malloc(TAILLE_MAX * sizeof(char));
-                     
-                     snprintf(ch,TAILLE_MAX,"pdftotext -layout listepdf/%s particulier.txt",fichier);
-                     system(ch);
-                     
-                     char * lien2;
-	             lien2 = (char *) malloc(TAILLE_MAX * sizeof(char));
-		     strcat(lien2, "resultat/particulier.txt");
-		     FILE * filep = fopen(lien2, "r");
-		     int nbLigneVide = 0;
-                     while(fgets(pars, TAILLE_MAX, filep) != NULL){
-                              char c;
-                              c = pars[0];
-                              if(nbLigneVide >= 2){
-                                 int b = 1;
-                                    while(b == 1){
-		                           if((strstr(ligne, "Introduction") || strstr(ligne, "INTRODUCTION") || strstr(ligne, "introduction")||strstr(ligne, "I NTRODUCTION"))){
-		                                    return buffer;
-		                           }
-		                           else{
-						strcat(buffer, ligne);
-						strcat(buffer, " ");
-		                            }
-                                    }   
-                              }
-                              if(c == '\n') nbLigneVide++;
-                              
-                              else{
-                                 nbLigneVide = 0;
-                              }
-                     }
-           }*/
-        
            if((strstr(ligne, "Introduction") || strstr(ligne, "INTRODUCTION") || strstr(ligne, "introduction")||strstr(ligne, "I NTRODUCTION"))){
                    break;
            }
@@ -236,40 +228,171 @@ char * trouver_auteurs(char * nomFichier){
            numLigne++;
          
        }
-         //   printf("les auteurs sont : %s \n",buffer);
              return buffer;
 }
+
+char * trouver_introduction(char * fichier){
+          char * buffer;
+          buffer = (char*)malloc(TAILLE_MAX * sizeof(char));
+       
+          char * lien;
+	  lien = (char *) malloc(TAILLE_MAX * sizeof(char));
+	  strcat(lien, "resultatRaw/");
+	  strcat(lien, fichier);
+      
+	  FILE * file = fopen(lien, "r");
+	       
+	  char * ligne;
+	  ligne = (char *) malloc(TAILLE_MAX * sizeof(char));
+       
+          int condition = 1;
+        while(fgets(ligne, TAILLE_MAX, file) != NULL){
+        
+            
+            char * newLigne;
+            newLigne = (char*)malloc(TAILLE_MAX * sizeof(char));
+            
+            if((ligne[0] == '2'|| ligne[1] == '2' || strstr(ligne, "II.")) && ( strstr(ligne, "2.") || strstr(ligne,"2 ") || strstr(ligne, "II.") )&& condition == 0 ){
+                 break;
+           }
+        
+           if((strstr(ligne, "Introduction") || strstr(ligne, "INTRODUCTION") || strstr(ligne, "introduction")||strstr(ligne, "I NTRODUCTION")|| condition == 0)){
+                   int taille = strlen(ligne);
+                   strncpy(newLigne, ligne, taille-1);
+                   strcat(buffer, newLigne);
+                   strcat(buffer, " ");
+                   condition = 0;
+           }
+       }
+            return buffer;
+}
+
+
+char * trouver_conclusion(char * fichier){
+
+          char * buffer;
+          buffer = (char*)malloc(TAILLE_MAX * sizeof(char));
+       
+          char * lien;
+	  lien = (char *) malloc(TAILLE_MAX * sizeof(char));
+	  strcat(lien, "resultatRaw/");
+	  strcat(lien, fichier);
+      
+	  FILE * file = fopen(lien, "r");
+	       
+	  char * ligne;
+	  ligne = (char *) malloc(TAILLE_MAX * sizeof(char));
+	  int i = 0, numLigne = 0;
+	  while(fgets(ligne, TAILLE_MAX, file) != NULL){
+	         if((strstr(ligne, "conclusions") || strstr(ligne, "conclusion") || strstr(ligne, "CONCLUSIONS")||strstr(ligne, "CONCLUSION"))) numLigne = i;
+	         else i++;
+	  }
+	  
+	  if(numLigne == 0) return NULL;
+	  
+	    char * ligne2;
+            ligne2 = (char*)malloc(TAILLE_MAX * sizeof(char));
+	  
+          int condition = 1;
+          int j = 0;
+        while(fgets(ligne2, TAILLE_MAX, file) != NULL){
+            
+            //char * newLigne;
+            //newLigne = (char*)malloc(TAILLE_MAX * sizeof(char));
+            
+            if( strstr(ligne2, "Acknowledgements") || strstr(ligne2, "ACKNOWLEDGEMENTS") || strstr( ligne2,"References")||strstr( ligne2,"REFERENCES") ||strstr( ligne2,"references") ||       
+                       strstr( ligne2,"R EFERENCES") ){
+                 break; 
+            }
+        
+            if(j>=numLigne && (strstr(ligne2, "conclusions") || strstr(ligne2, "conclusion") || strstr(ligne2, "CONCLUSIONS")||strstr(ligne2, "CONCLUSION")|| condition == 0)){
+            
+                 /* if(strstr(ligne2, "conclusions") || strstr(ligne2, "conclusion") || strstr(ligne2, "CONCLUSIONS")||strstr(ligne2, "CONCLUSION")){
+                         
+                  }*/
+                 
+               /*    int taille = strlen(ligne2);
+                   strncpy(newLigne, ligne2, taille-1);*/
+                   strcat(buffer, ligne2);
+                   strcat(buffer, " ");
+                   condition = 0;
+           }
+         
+           j++;
+       }
+          
+            return buffer;
+}
+
+/*char * trouver_discussion(char * fichier){
+
+          char * buffer;
+          buffer = (char*)malloc(TAILLE_MAX * sizeof(char));
+       
+          char * lien;
+	  lien = (char *) malloc(TAILLE_MAX * sizeof(char));
+	  strcat(lien, "resultatRaw/");
+	  strcat(lien, fichier);
+      
+	  FILE * file = fopen(lien, "r");
+	       
+	  char * ligne;
+	  ligne = (char *) malloc(TAILLE_MAX * sizeof(char));
+       
+          int condition = 1;
+        while(fgets(ligne, TAILLE_MAX, file) != NULL){
+        
+            
+            char * newLigne;
+            newLigne = (char*)malloc(TAILLE_MAX * sizeof(char));
+            
+            if(strstr(ligne, "Acknowledgements") || strstr(ligne, "ACKNOWLEDGEMENTS") || strstr( ligne,"References")||strstr( ligne,"REFERENCES") ||strstr( ligne,"references") ||       
+                       strstr( ligne,"R EFERENCES") || strstr(ligne, "conclusions") || strstr(ligne, "conclusion") || strstr(ligne, "CONCLUSIONS")||strstr(ligne, "CONCLUSION") ){
+                 condition = 1;
+                
+           }
+        
+            if(strstr(ligne, "discussion") || strstr(ligne, "discussions") || strstr(ligne, "DISCUSSIONS")||strstr(ligne, "DISCUSSION")|| condition == 0){
+            
+                //    if(strstr(ligne, "discussion") || strstr(ligne, "discussions") || strstr(ligne, "DISCUSSIONS")||strstr(ligne, "DISCUSSION")) buffer = NULL;
+                 
+                   int taille = strlen(ligne);
+                   strncpy(newLigne, ligne, taille-1);
+                   strcat(buffer, newLigne);
+                   strcat(buffer, " ");
+                   condition = 0;
+           }
+       }
+            return buffer;
+}
+*/
 
 
 char* trouver_biblio(char * fichier){
  
- char * lien;
- lien = (char *) malloc(TAILLE_MAX * sizeof(char));
- strcat(lien, "resultat/");
- strcat(lien, fichier);
+	 char * lien;
+	 lien = (char *) malloc(TAILLE_MAX * sizeof(char));
+	 strcat(lien, "resultat/");
+	 strcat(lien, fichier);
 
 
- FILE * file = fopen(lien, "r");
- char * ligne;
- ligne = (char *) malloc(TAILLE_MAX * sizeof(char));
+	 FILE * file = fopen(lien, "r");
+	 char * ligne;
+	 ligne = (char *) malloc(TAILLE_MAX * sizeof(char));
 
- char * para;
- para = (char *) malloc(TAILLE_MAX * sizeof(char));
- 
- int condition = 0;
- while (fscanf(file, "%[^\n] ", ligne) != EOF){
- // char * ligneOrigine = ligne;
- // char * ligneCopie = retirerEspace(ligne);
- 
-
- 
- if(strcmp( ligne,"References")== 0 ||strcmp( ligne,"REFERENCES")== 0 ||strcmp( ligne,"references")== 0 ||strcmp( ligne,"R EFERENCES")== 0 ||condition == 1 ){
- strcat(para,ligne);
- condition=1;
- 
- }
- } 
- return para;
+	 char * para;
+	 para = (char *) malloc(TAILLE_MAX * sizeof(char));
+	 
+	 int condition = 0;
+	 while (fscanf(file, "%[^\n] ", ligne) != EOF){
+	 
+		 if(strcmp( ligne,"References")== 0 ||strcmp( ligne,"REFERENCES")== 0 ||strcmp( ligne,"references")== 0 ||strcmp( ligne,"R EFERENCES")== 0 ||condition == 1 ){
+			 strcat(para,ligne);
+			 condition=1;
+		 
+		 }
+	 } 
+	 return para;
 }
 void execute(int x)
 {   
@@ -282,6 +405,8 @@ void execute(int x)
 
          listerPdf(); // converti tous les fichier pdf en txt et les mettre dans un dossier "resultat"
          listerPdfWithOptionLayout();
+         listerPdfWithOptionRaw();
+         
          system("ls resultat/>test.txt ");  // crée un fichier "test.txt" qui contient les non de tous les fichier.txt
          FILE* fichier = fopen("test.txt", "r");
   
@@ -292,6 +417,17 @@ void execute(int x)
                 char * saut;
                 buffer3 = (char*)malloc(TAILLE_MAX * sizeof(char));
                 saut = (char*)malloc(TAILLE_MAX * sizeof(char));
+                
+                char * titre = trouver_titre(chaine);         // mettre le titre du fichier dans titre
+                char * biblio = trouver_biblio(chaine);         // mettre la biblio du fichier dans titre
+                char * abstract = trouver_abstract(chaine);  // mettre le resumé du fichier dans abstract
+                char * auteurs = trouver_auteurs(chaine);  // mettre les auteur du fichier dans auteurs
+                char * intro = trouver_introduction(chaine); // mettre l'intro du fichier dans intro
+                char * conclusion = trouver_conclusion(chaine);
+               // char * discussion = trouver_discussion(chaine);
+               
+              // if(conclusion == NULL) conclusion = "il n y a pas de conclusion";
+                
                 if(x==1)
                 {
                 snprintf(buffer3,TAILLE_MAX,"touch -f %s",chaine);
@@ -310,11 +446,6 @@ void execute(int x)
                 snprintf(saut,TAILLE_MAX,"echo >> %s ",chaine);
                 system(buffer2);
                 system(saut);
-                
-                char * titre = trouver_titre(chaine);         // mettre le titre du fichier dans titre
-                char * biblio = trouver_biblio(chaine);         // mettre la biblio du fichier dans titre
-                char * abstract = trouver_abstract(chaine);  // mettre le resumé du fichier dans abstract
-                char * auteurs = trouver_auteurs(chaine);  // mettre les auteur du fichier dans auteurs
                 
                 // ajouter le titre du fichier
                 char * buffer;
@@ -336,6 +467,27 @@ void execute(int x)
                 snprintf(buffer1,TAILLE_MAX,"echo  \"%s\" >> %s",abstract,chaine);
                 system(buffer1);
                 system(saut);
+                
+                // ajouter l'intro du fichier
+                char * buffer12;
+                buffer12 = (char*)malloc(TAILLE_MAX * sizeof(char));
+                snprintf(buffer12,TAILLE_MAX,"echo l introduction est : \"%s\" >> %s",intro,chaine);
+                system(buffer12);
+                system(saut);
+                
+                // ajouter la conclusion du fichier
+                char * buffer13;
+                buffer13 = (char*)malloc(TAILLE_MAX * sizeof(char));
+                snprintf(buffer13,TAILLE_MAX,"echo la conclusion est : \"%s\" >> %s",conclusion,chaine);
+                system(buffer13);
+                system(saut);
+                
+                // ajouter la discussion du fichier
+              /*  char * buffer14;
+                buffer14 = (char*)malloc(TAILLE_MAX * sizeof(char));
+                snprintf(buffer14,TAILLE_MAX,"echo la discussion est : \"%s\" >> %s",discussion,chaine);
+                system(buffer14);
+                system(saut);*/
                 
                 // ajouter la biblio du fichier
                 char * buffer11;
@@ -372,6 +524,7 @@ void execute(int x)
                 snprintf(bufferx,TAILLE_MAX,"echo \"<article>\" > %s",chaine);
                 system(bufferx);
                 
+                
                 // ajouter le nom original du fichier (.pdf)
                 char * buffer2;
                 buffer2 = (char*)malloc(TAILLE_MAX * sizeof(char));
@@ -380,10 +533,6 @@ void execute(int x)
                 
                 chaine = strtok(chaine,".");
                 strcat(chaine,".txt");
-                char * titre = trouver_titre(chaine);         // mettre le titre du fichier dans titre
-                char * biblio = trouver_biblio(chaine);         // mettre la biblio du fichier dans titre
-                char * auteurs = trouver_auteurs(chaine);
-                char * abstract = trouver_abstract(chaine);  // mettre le resumé du fichier dans abstract
                 chaine = strtok(chaine,".");
                 strcat(chaine,".xml");
                 
@@ -408,6 +557,27 @@ void execute(int x)
                 system(buffer1);
                  system(saut);
                  
+                 // ajouter l'intro du fichier
+                char * buffer12;
+                buffer12 = (char*)malloc(TAILLE_MAX * sizeof(char));
+                snprintf(buffer12,TAILLE_MAX,"echo  \"<introduction> %s </introduction>\" >> %s",intro,chaine);
+                system(buffer12);
+                system(saut);
+                
+                // ajouter la conclusion du fichier
+                char * buffer13;
+                buffer13 = (char*)malloc(TAILLE_MAX * sizeof(char));
+                snprintf(buffer13,TAILLE_MAX,"echo la conclusion est : \"%s\" >> %s",conclusion,chaine);
+                system(buffer13);
+                system(saut);
+                
+                // ajouter la discussion du fichier
+                /*char * buffer14;
+                buffer14 = (char*)malloc(TAILLE_MAX * sizeof(char));
+                snprintf(buffer14,TAILLE_MAX,"echo la discussion est : \"%s\" >> %s",discussion,chaine);
+                system(buffer14);
+                system(saut);*/
+                 
                 // ajouter la biblio du fichier
                 char * buffer11;
                 buffer11 = (char*)malloc(TAILLE_MAX * sizeof(char));
@@ -430,7 +600,8 @@ void execute(int x)
         }
                 fclose(fichier);  
                 system("rm test.txt "); 
-                
+                system("rm -fr resultatLayout");  // supprimer le dossier resultatLayout si il existe
+                system("rm -fr resultatRaw");  // supprimer le dossier resultatRaw si il existe
 }
 
 int main()
